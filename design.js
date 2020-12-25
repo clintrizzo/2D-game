320781d381ead525073ae5dfc4e937de40c6aeec
Clint <maxeva02@gmail.com>
2020-12-25 14:45:48 -0800

    deleted brick code and updated the code for the ball

--- design.js
+++ design.js
@@ -1,70 +1,26 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-//testing the design inside the context box
-//ctx.beginPath();
-// ctx.rect(20, 40, 50, 50);
-// ctx.fillStyle = "#FF0000";
-// ctx.fill();
-// ctx.closePath();
-
-// ctx.beginPath();
-// ctx.arc(240, 160, 20, 0, Math.PI * 2);
-// ctx.fillStyle = "green";
-// ctx.fill();
-// ctx.closePath();
-
-// ctx.beginPath();
-// ctx.rect(160, 10, 100, 40);
-// ctx.strokeStyle = "rgba(0,0, 255, 0.5)"; // stroke style is to fill the outer boxes like border in css
-// ctx.stroke();
-// ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(10, 2, 84, 20);
-ctx.fillStyle = "#ff0000";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(104, 2, 84, 20);
-ctx.fillStyle = "blue";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(198, 2, 84, 20);
-ctx.fillStyle = "#ff0000";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(292, 2, 84, 20);
-ctx.fillStyle = "blue";
-ctx.fill();
-ctx.closePath();
-
-ctx.beginPath();
-ctx.rect(386, 2, 84, 20);
-ctx.fillStyle = "#ff0000";
-ctx.fill();
-ctx.closePath();
-
-
 //code for the ball
+
 var x = canvas.width / 2;
 var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095dd";
+    ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     x += dx;
     y += dy;
 }
+
 setInterval(draw, 10);
