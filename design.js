34f5725f4cc032311d73dcf08045ddb0aebbea2c
Clint <maxeva02@gmail.com>
2020-12-25 17:45:15 -0800

    created code for the paddle

--- design.js
+++ design.js
@@ -1,12 +1,17 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
-//code for the ball
 
+//code for the ball
 var x = canvas.width / 2;
 var y = canvas.height - 30;
-var dx = 0;
+var dx = 2;
 var dy = -2;
+
+//paddle variables
+var paddleHeight = 10;
+var paddleWidth = 70;
+var paddleX = (canvas.width - paddleWidth) / 2;
 
 function drawBall() {
     ctx.beginPath();
@@ -15,9 +20,14 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
     ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    if (y + dy > canvas.height || y + dy < 0) {
-        dy = -dy;
-    }
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#grey";
+    ctx.fill();
+    ctx.closePath();
 }
 
 function draw() {
@@ -25,6 +35,15 @@ function draw() {
     drawBall();
     x += dx;
     y += dy;
+    if (y + dy > canvas.height || y + dy < 0) { //ball to bounce off the top and the bottom of the edges
+        dy = -dy;
+    }
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy
+    }
 }
 
 
