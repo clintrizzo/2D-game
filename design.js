c4bdb60b1ea71b930cd4b16ecb317c60c68b257b
Clint <maxeva02@gmail.com>
2020-12-27 11:04:27 -0800

    added more detailed comments for the javascript

--- design.js
+++ design.js
@@ -9,7 +9,7 @@ var dx = 4;
 var dy = -2;
 
 //paddle variables
-var paddleHeight = 10;
+var paddleHeight = 5;
 var paddleWidth = 85;
 var paddleX = (canvas.width - paddleWidth) / 2;
 
@@ -62,6 +62,7 @@ function keyUpHandler(e) {
     }
 }
 
+//code for the collision from the ball to the bricks
 function collisionDetection() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
@@ -70,9 +71,10 @@ function collisionDetection() {
                 if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
                     dy = -dy;
                     b.status = 0;
-                    score++;
-                    if (score == brickRowCount * brickColumnCount) {
+                    score++; // having the score increase once each brick is hit
+                    if (score == brickRowCount * brickColumnCount) { //game will end once all bricks are destroyed
                         alert("You Win, Congratulations!");
+                        //reloading the game once the user destroys all of the bricks
                         document.location.reload();
                         clearInterval(interval);
                     }
@@ -82,26 +84,30 @@ function collisionDetection() {
     }
 }
 
+//design for the score on the top left corner
 function drawScore() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#c4c7ce;"
+    ctx.fillStyle = "white";
     ctx.fillText("Score" + score, 25, 20);
 }
 
+//design for the lives on the top right corner
 function drawLives() {
     ctx.font = "16px Arial";
-    ctx.fillStyle = "#c4c7ce";
+    ctx.fillStyle = "white";
     ctx.fillText("Lives: " + lives, canvas.width - 95, 20);
 }
 
+//drawing the ball / design of the ball 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2); //enables the ball to move two spaces per ms so it doesnt leave a trail
     ctx.fillStyle = "#c4c7ce";
     ctx.fill();
     ctx.closePath();
 }
 
+//design for the bottom paddle
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
@@ -111,6 +117,7 @@ function drawPaddle() {
 }
 
 function drawBricks() {
+    //detailing how the bricks are to be set up per row and column
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             if (bricks[c][r].status == 1) {
@@ -118,7 +125,7 @@ function drawBricks() {
                 var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
-                ctx.beginPath();
+                ctx.beginPath(); //design of the bricks
                 ctx.rect(brickX, brickY, brickWidth, brickHeight);
                 ctx.fillStyle = "#blue";
                 ctx.fill();
@@ -129,6 +136,7 @@ function drawBricks() {
     }
 }
 
+//complete code calling the functions of the designed segments above
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
@@ -161,14 +169,16 @@ function draw() {
         }
     }
 
+    //declaring the speed of the paddle
     if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 7;
+        paddleX += 10;
     } else if (leftPressed && paddleX > 0) {
-        paddleX -= 7;
+        paddleX -= 10;
     }
 
     x += dx;
     y += dy;
 }
 
+//reseting the game if user wins
 var interval = setInterval(draw, 10);
