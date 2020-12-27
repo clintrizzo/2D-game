e4d4ca85c7105f07e56c97fea204e6c0a52af0df
Clint <maxeva02@gmail.com>
2020-12-26 22:51:03 -0800

    added lives and a reload to the page when the ball hits the bottom of the board

--- design.js
+++ design.js
@@ -91,7 +91,7 @@ function drawScore() {
 function drawLives() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#c4c7ce";
-    ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+    ctx.fillText("Lives: " + lives, canvas.width - 95, 20);
 }
 
 function drawBall() {
@@ -136,26 +136,29 @@ function draw() {
     drawBricks();
     collisionDetection();
     drawScore();
+    drawLives();
     //getting the game to end when hitting the bottom of the board
     if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
     if (y + dy < ballRadius) {
         dy = -dy;
-    } else if (y + dy > canvas.height) {
+    } else if (y + dy > canvas.height - ballRadius) {
         if (x > paddleX && x < paddleX + paddleWidth) {
             dy = -dy;
         } else {
-            alert("GAME OVER");
-            document.location.reload();
-            clearInterval(interval); // Needed for Chrome to end game
+            lives--;
+            if (!lives) {
+                alert("GAME OVER!!");
+                document.location.reload();
+            } else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 3;
+                dy = -3;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
         }
-    }
-
-    if (rightPressed && paddleX < canvas.width - paddleWidth) {
-        paddleX += 7;
-    } else if (leftPressed && paddleX > 0) {
-        paddleX -= 7;
     }
 
     x += dx;
